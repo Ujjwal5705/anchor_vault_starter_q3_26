62575375f5ba8dee2a53e77ce64e86df4fff2e75
Ujjwal Sharma <sharmaujjwal5705@gmail.com>
2026-09-09 18:57:27 +0530

    Setup Escrow and Scaffolding

---
+++ programs/escrow/src/lib.rs
@@ -0,0 +1,33 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("A1csyUJARLXE5ygQ5V3mEgVSJqsa1QXcYjnpppaTY9hU");
+
+#[program]
+pub mod escrow {
+    use super::*;
+
+    pub fn make(ctx: Context<Make>, seed: u64, receive: u64, deposit: u64) -> Result<()> {
+        ctx.accounts.make(seed, receive, deposit, &ctx.bumps)
+    }
+
+    // pub fn take(ctx: Context<Take>) -> Result<()> {
+    //     ctx.accounts.take()
+    // }
+
+    // pub fn refund(ctx: Context<Refund>) -> Result<()> {
+    //     ctx.accounts.refund()
+    // }
+
+    // pub fn update(ctx: Context<Update>, receive: u64) -> Result<()> {
+    //     ctx.accounts.update(receive)
+    // }
+}
